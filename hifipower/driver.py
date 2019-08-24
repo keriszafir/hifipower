03dfc5a0f7e021d6859135d76550738c1ec0e74d
elegantandrogyne <krzysztof.slychan@gmail.com>
2019-08-24 15:43:42 +0200

    GPIO control and rudimentary web API

--- hifipower/driver.py
+++ hifipower/driver.py
@@ -3,23 +3,28 @@
 Can be used with Orange Pi (the original device is based on OPi Plus,
 as it has gigabit Ethernet port and SATA controller),
 or a regular Raspberry Pi"""
+import atexit
+import subprocess
+from contextlib import suppress
 
 try:
     # use SUNXI as it gives the most predictable results
     import OPi.GPIO as GPIO
     GPIO.setmode(GPIO.SUNXI)
+    GPIO_DEFINITIONS = dict(relay_out='PA7', auto_mode_in='PA8',
+                            shutdown_button='PA9', reboot_button='PA10')
     print('Using OPi.GPIO on an Orange Pi with the SUNXI numbering.')
 
 except ImportError:
     # use BCM as it is the most conventional scheme on a RPi
     import RPi.GPIO as GPIO
     GPIO.setmode(GPIO.BCM)
+    GPIO_DEFINITIONS = dict(relay_out=5, auto_mode_in=6,
+                            shutdown_button=13, reboot_button=19)
     print('Using RPi.GPIO on a Raspberry Pi with the BCM numbering.')
 
 
 ON, OFF = True, False
-GPIO_DEFINITIONS = dict(auto_mode_in=None, control_out=None,
-                        shutdown_button=None, reboot_button=None)
 
 
 class AutoControlDisabled(Exception):
@@ -28,54 +33,81 @@ class AutoControlDisabled(Exception):
     """
 
 
-def gpio_setup(gpio_definitions):
+def gpio_setup(config):
     """Reads the gpio definitions dictionary,
     sets the outputs and inputs accordingly."""
+    def shutdown():
+        """Shut the system down"""
+        command = 'sudo systemctl poweroff'
+        subprocess.run([x.strip() for x in command.split(' ')])
+
+    def reboot():
+        """Restart the system"""
+        command = 'sudo systemctl reboot'
+        subprocess.run([x.strip() for x in command.split(' ')])
+
     # update the defaults with values from config
-    GPIO_DEFINITIONS.update(gpio_definitions)
+    gpio_defs = {k: v for k, v in config.items() if k in GPIO_DEFINITIONS}
 
-    gpios = dict(auto_mode_in=GPIO.IN,
-                 control_out=GPIO.OUT,
-                 shutdown_button=(GPIO.IN, GPIO.PUD_UP),
-                 reboot_button=(GPIO.IN, GPIO.PUD_UP))
+    gpios = dict(relay_out=dict(direction=GPIO.OUT),
+                 auto_mode_in=dict(direction=GPIO.IN),
+                 shutdown_button=dict(direction=GPIO.IN,
+                                      pull_up_down=GPIO.PUD_UP),
+                 reboot_button=dict(direction=GPIO.IN,
+                                    pull_up_down=GPIO.PUD_UP))
+    callbacks = dict(shutdown_button=shutdown,
+                     reboot_button=reboot)
 
-    for gpio_name, gpio_id in gpio_definitions.items():
+    for gpio_name, gpio_id in gpio_defs.items():
         if gpio_id is None:
             # skip the undefined GPIO
             continue
 
+        parameters = dict()
         try:
-            # GPIO pin defined with a pull-up / pull-down argument
-            direction, pull_up_down = gpios[gpio_name]
-            GPIO.setup(gpio_id, direction, pull_up_down=pull_up_down)
-        except TypeError:
-            # no pull-up/pull-down info
-            direction = gpios[gpio_name]
-            GPIO.setup(gpio_id, direction)
+            parameters.update(gpios[gpio_name])
         except KeyError:
-            # trying to set up an unregistered GPIO
+            # not found in gpios: unknown GPIO, or no GPIO definition at all
             continue
 
+        GPIO.setup(gpio_id, **parameters)
+        # update the value in main storage
+        GPIO_DEFINITIONS[gpio_name] = gpio_id
+        # set up a threaded callback if needed
+        with suppress(KeyError):
+            callback_function = callbacks[gpio_id]
+            GPIO.add_event_detect(gpio_id, GPIO.RISING, bouncetime=1000,
+                                  callback=callback_function)
 
-def automatic_mode():
+    # force to clean up the definitions
+    atexit.register(GPIO.cleanup)
+
+
+def check_automatic_mode():
     """Checks if the device is in automatic control mode"""
     channel = GPIO_DEFINITIONS['auto_mode_in']
     return GPIO.input(channel)
 
 
-def output_control(state):
+def check_output_state():
+    """Checks the output state"""
+    channel = GPIO_DEFINITIONS['relay_out']
+    return GPIO.input(channel)
+
+
+def set_output(state):
     """Controls the state of the output"""
-    if not automatic_mode():
+    if not check_automatic_mode():
         raise AutoControlDisabled
-    channel = GPIO_DEFINITIONS['control_out']
+    channel = GPIO_DEFINITIONS['relay_out']
     GPIO.output(channel, state)
 
 
 def turn_on():
     """Turns the power ON"""
-    output_control(ON)
+    set_output(ON)
 
 
 def turn_off():
     """Turns the power OFF"""
-    output_control(OFF)
+    set_output(OFF)
--- hifipower/main.py
+++ hifipower/main.py
@@ -21,5 +21,63 @@ Auto power-on is also planned. Whenever the sound card's active
 turned on. This may be problematic though, in case a longer time is needed
 for the switch-on (e.g. when using a vacuum tube power amp).
 """
+import logging
+import signal
+import sys
+from configparser import ConfigParser
+from contextlib import suppress
+from flask import Flask
 from . import driver
 
+LOG = logging.getLogger('hifipowerd')
+CFG = ConfigParser(defaults=dict(auto_mode_in='PA7',
+                                 control_out='PA8',
+                                 shutdown_button='PA9',
+                                 reboot_button='PA10'))
+CFG.read('/etc/hifipowerd.conf')
+
+
+def journald_setup():
+    """Set up and start journald logging"""
+    debug_mode = CFG.defaults().get('debug_mode')
+    if debug_mode:
+        LOG.setLevel(logging.DEBUG)
+        LOG.addHandler(logging.StreamHandler(sys.stderr))
+    with suppress(ImportError):
+        from systemd.journal import JournaldLogHandler
+        journal_handler = JournaldLogHandler()
+        log_entry_format = '[%(levelname)s] %(message)s'
+        journal_handler.setFormatter(logging.Formatter(log_entry_format))
+        LOG.setLevel(logging.INFO)
+        LOG.addHandler(journal_handler)
+
+
+def webapi():
+    """JSON web API for communicating with the casting software."""
+    def index():
+        """Get or change the interface's current status."""
+        return dict(power=driver.check_output_state(),
+                    auto_mode=driver.check_automatic_mode())
+
+    app = Flask('rpi2casterd')
+    app.route('/power')(index)
+    app.route('/power/on')(driver.turn_on)
+    app.route('/power/off')(driver.turn_off)
+    config = CFG.defaults()
+    app.run(config.get('address'), config.get('port'),
+            debug=config.get('debug_mode'))
+
+
+def main():
+    """Main function"""
+    # signal handling routine
+    def signal_handler(*_):
+        """Exit gracefully if SIGINT or SIGTERM received"""
+        raise KeyboardInterrupt
+
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
+    # get the GPIO definitions and set up the I/O
+    journald_setup()
+    driver.gpio_setup(CFG.defaults())
