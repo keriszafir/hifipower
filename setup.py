2932f52da12ce7751ff08c0067da34b3bb414654
elegantandrogyne <krzysztof.slychan@gmail.com>
2020-09-26 17:45:41 +0200

    version bump and author name change

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 """hifipower setup, meant for Raspberry Pi or Orange Pi"""
 from setuptools import setup
 
-__version__ = '0.1b7'
-__author__ = 'Christophe Catherine Slychan'
-__author_email__ = 'krzysztof.slychan@gmail.com'
+__version__ = '0.1'
+__author__ = 'Keri Szafir'
+__author_email__ = 'keri.szafir@gmail.com'
 __github_url__ = 'http://github.com/elegantandrogyne/hifipower'
 __dependencies__ = ['Flask >= 1.0.2']
 
