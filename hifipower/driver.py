9d511f7120c847d5106a00e3080f8bf486f91f3e
elegantandrogyne <krzysztof.slychan@gmail.com>
2020-09-27 07:38:29 +0200

    rework LED functionality

--- hifipower/driver.py
+++ hifipower/driver.py
@@ -12,8 +12,8 @@ try:
     # use SUNXI as it gives the most predictable results
     import OPi.GPIO as GPIO
     GPIO.setmode(GPIO.SUNXI)
-    GPIO_DEFINITIONS = dict(onoff_button='PC4', mode_led='PC7',
-                            relay_out='PA7', auto_mode_in='PA8',
+    GPIO_DEFINITIONS = dict(onoff_button='PA7', mode_led='PC4',
+                            relay_out='PC7', auto_mode_in='PA8',
                             shutdown_button='PA9', reboot_button='PA10',
                             relay_state='PA11', ready_led='PA12')
     print('Using OPi.GPIO on an Orange Pi with the SUNXI numbering.')
@@ -23,8 +23,8 @@ except ImportError:
     # use BCM as it is the most conventional scheme here
     import RPi.GPIO as GPIO
     GPIO.setmode(GPIO.BCM)
-    GPIO_DEFINITIONS = dict(onoff_button=23, mode_led=4,
-                            relay_out=5, auto_mode_in=6,
+    GPIO_DEFINITIONS = dict(onoff_button=5, mode_led=23,
+                            relay_out=24, auto_mode_in=6,
                             shutdown_button=13, reboot_button=19,
                             relay_state=3, ready_led=2)
     print('Using RPi.GPIO on a Raspberry Pi with the BCM numbering.')
@@ -43,27 +43,30 @@ def gpio_setup(config):
     sets the outputs and inputs accordingly."""
     def shutdown():
         """Shut the system down"""
+        led('mode_led', OFF, blink=2)
         command = config.get('shutdown_command', 'poweroff')
         subprocess.run([x.strip() for x in command.split(' ')])
 
     def reboot():
         """Restart the system"""
+        led('mode_led', OFF, blink=2)
         command = config.get('reboot_command', 'reboot')
         subprocess.run([x.strip() for x in command.split(' ')])
 
     def toggle_state():
         """Turn the power on or off after pressing the on/off button,
         depending on the previous state"""
-        set_relay(not check_state('relay_out'))
+        led('mode_led', blink=2)
+        relay(not check_state('relay_out'))
 
     def check_auto_mode():
         """When the automatic control mode is on, turn on the red LED,
         when it's off, turn the LED off"""
-        set_led('mode_led', check_state('auto_mode_in'))
+        led('mode_led', check_state('auto_mode_in'))
 
     def finish():
         """Blink a LED and then clean the GPIO"""
-        set_led('ready_led', OFF, blink=5)
+        led('ready_led', OFF, blink=5)
         GPIO.cleanup()
 
     gpios = dict(onoff_button=GPIO.IN, mode_led=GPIO.OUT,
@@ -95,7 +98,7 @@ def gpio_setup(config):
 
     # flash a LED and clean up the definitions
     atexit.register(finish)
-    set_led('ready_led', ON)
+    led('ready_led', ON, blink=2, duration=0.2)
 
 
 def check_state(gpio_name):
@@ -104,7 +107,7 @@ def check_state(gpio_name):
     return GPIO.input(channel)
 
 
-def set_relay(state):
+def relay(state):
     """Controls the state of the power relay"""
     if not check_state('auto_mode_in'):
         raise AutoControlDisabled
@@ -112,14 +115,24 @@ def set_relay(state):
     GPIO.output(channel, state)
 
 
-def set_led(gpio_name, state=False, blink=0):
-    """Set the LED state"""
+def led(gpio_name, state=None, blink=0, duration=0.5):
+    """LED control:
+        state - 0/1, True/False - sets the new state;
+                None preserves the previous one
+        blink - number of LED blinks before the state is set
+        duration - total time of blinking in seconds"""
     channel = GPIO_DEFINITIONS[gpio_name]
+    # preserve the previous state in case it is None
+    if state is None:
+        state = check_state(gpio_name)
+    # each blink cycle has 2 timesteps,
+    # how long they are depends on the number of cycles and blinking duration
+    timestep = 0.5 * duration / (blink or 1)
     # blinking a number of times
     for _ in range(blink):
         GPIO.output(channel, ON)
-        time.sleep(0.2)
+        time.sleep(timestep)
         GPIO.output(channel, OFF)
-        time.sleep(0.2)
+        time.sleep(timestep)
     # final state
     GPIO.output(channel, state)
